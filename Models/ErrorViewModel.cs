f9ebf8fef53b09fc657a9af93a7f2b750cdced49
Drew <drewharper29@gmail.com>
2021-02-28 11:58:57 -0500

    init commit, added accounts controller and model

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace webad_source.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
